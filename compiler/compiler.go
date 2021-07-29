b1424d04db097fc1fc69300f2142ea8fc0f67dac
Cesar Gimenes <crg@crg.eti.br>
2021-07-29 10:39:49 -0300

    add mock compiler

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,8 +1,19 @@
 package compiler
 
+import (
+	"fmt"
+	"io"
+)
+
 type Compiler struct {
 }
 
 func New() *Compiler {
 	return &Compiler{}
+}
+
+func (c *Compiler) CompileFile(file string, w io.Writer) error {
+	fmt.Println(file)
+	w.Write([]byte("teste compile writer\r\n"))
+	return nil
 }
